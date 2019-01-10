56cf6dcbf6ab7f4182e7b5e36bc372e5cc07dc1a
Ajay Pillarisetti <ajaypillarisetti@berkeley.edu>
2019-01-10 10:12:25 -0800

    add tests

--- R/utils.R
+++ R/utils.R
@@ -9,7 +9,6 @@ f_to_c <- function(F) {
 }
 
 ################################################################################
-
 #' Generic SUMs import functions
 #'
 #' Uses string parsing to guess which type of sensor
@@ -36,15 +35,43 @@ import_sums <- function(input_file, ...) {
 		message("Input File ", basename(input_file), " is not recognized as a valid SUMs file. Manually import.")
 	}else{
 		if(any(unlist(lapply(input_header$V1, function(x) { grepl('iButton|### ', x) })))) {
-			read.ibutton(input_file, ...)
+			read_ibutton(input_file, ...)
 		} else if(any(unlist(lapply(input_header$V1, function(x) { grepl('kSUM', x) })))) {
-			read.ksums(input_file, ...)
+			read_ksums(input_file, ...)
 		} else if(any(unlist(lapply(input_header$V1, function(x) { grepl('Celsius\\(', x, perl = T) })))) {
-			read.lascar(input_file, ...)
+			read_lascar(input_file, ...)
 		} else if(any(unlist(lapply(input_header$V1, function(x) { grepl('Index,Timestamp,Thermocouple', x) })))){
-			read.wellzion(input_file, ...)
+			read_wellzion(input_file, ...)
 		} else {
 			message("Input File ", basename(input_file), " is not recognized as a valid SUMs file. Manually import.")
 		}
+	}
+}
+
+################################################################################
+#' Read labeled files from LABLR
+#'
+#' Imports labeled files exported from LABLR
+#' and merges with metadata if specified for further use by SUMSarizer
+#' 
+#' @param input_file Path to file being imported
+#' @param metadata Path to file containing metadata for imported files or NA
+#' @return A data.frame with at least four columns - timestamp (POSIXct), value (numeric), label (logical), filename (character)
+#' 
+#' @export
+
+import_lablr <- function(input_file, metadata = NA, ...) {
+
+	labeled_file <- read.csv(input_file, stringsAsFactors = F, header = T)
+	labeled_file$timestamp <- ymd_hms(labeled_file$timestamp)
+	labeled_file$value <- as.numeric(labeled_file$value)
+
+	if(!is.na(metadata)){
+		long_metadata <- read.csv(metadata, stringsAsFactors = F, header = T)
+		wide_metadata <- reshape(long_metadata, timevar = "variable", idvar = 'filename', direction = 'wide')
+		names(wide_metadata) <- gsub("value.", "", names(wide_metadata))
+		merge(labeled_file, wide_metadata, by='filename')
+	}else{
+		labeled_file
 	}
 }
